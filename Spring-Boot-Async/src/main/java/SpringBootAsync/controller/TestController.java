11f47a151c1f981bcb9b47ce865517fe74c50619
liusj <15114818657@163.com>
2019-08-12 18:36:23 +0800

    Spring-Boot-Async

--- Spring-Boot-Async/src/main/java/SpringBootAsync/controller/TestController.java
+++ Spring-Boot-Async/src/main/java/SpringBootAsync/controller/TestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by liusj on 2019/8/6
@@ -61,5 +63,21 @@ public class TestController {
         logger.info("总耗时：{}ms", end - start);
 
         return stringFuture.get();
+    }
+
+    @GetMapping("/async3")
+    public String testAsync3() throws ExecutionException, InterruptedException, TimeoutException {
+        long start = System.currentTimeMillis();
+        logger.info("异步方法开始2");
+
+        Future<String> stringFuture = testService.asyncMethod2();
+        logger.info("异步方法返回值：" + stringFuture.get(0, TimeUnit.SECONDS));
+
+        logger.info("异步方法结束2");
+
+        long end = System.currentTimeMillis();
+        logger.info("总耗时：{}ms", end - start);
+
+        return stringFuture.get(0, TimeUnit.SECONDS);
     }
 }
