2d9ab82054ad47c91e324ad673d2f5f36314b548
liusj <15114818657@163.com>
2019-08-22 19:06:28 +0800

    Spring-Security-OAuth2-Guide

--- Spring-Security-OAuth2-Guide/src/main/java/top/lconcise/config/AuthorizationServerConfig.java
+++ Spring-Security-OAuth2-Guide/src/main/java/top/lconcise/config/AuthorizationServerConfig.java
@@ -1,21 +1,35 @@
 package top.lconcise.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
- * Created by liusj on 2019/8/1
+ * Created by liusj on 2019/8/19
  */
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory() // 使用in-memory存储
+                .withClient("test") // client_id
+                .secret("test1234") // client_secret
+                .redirectUris("https://www.baidu.com")
+                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
+                .scopes("all"); // 允许的授权范围
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients().passwordEncoder(passwordEncoder);
     }
 }
--- Spring-Security-OAuth2-Guide/src/main/java/top/lconcise/config/ResourceServerConfig.java
+++ Spring-Security-OAuth2-Guide/src/main/java/top/lconcise/config/ResourceServerConfig.java
@@ -1,13 +1,25 @@
 package top.lconcise.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
- * @author MrBird
+ * Created by liusj on 2019/8/1
  */
-//@Configuration
+@Configuration
 //@EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }
