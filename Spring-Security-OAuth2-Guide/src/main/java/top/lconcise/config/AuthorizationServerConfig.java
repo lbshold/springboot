e332ea90409ad73aaf249f66331e8313916a63d1
liusj <15114818657@163.com>
2019-08-27 09:10:33 +0800

    SpringSecurity-OAuth2-Guide

--- Spring-Security-OAuth2-Guide/src/main/java/top/lconcise/config/AuthorizationServerConfig.java
+++ Spring-Security-OAuth2-Guide/src/main/java/top/lconcise/config/AuthorizationServerConfig.java
@@ -1,35 +1,21 @@
 package top.lconcise.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
- * Created by liusj on 2019/8/19
+ * Created by liusj on 2019/8/26
  */
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory() // 使用in-memory存储
-                .withClient("test") // client_id
-                .secret("test1234") // client_secret
-                .redirectUris("https://www.baidu.com")
-                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
-                .scopes("all"); // 允许的授权范围
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.allowFormAuthenticationForClients().passwordEncoder(passwordEncoder);
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
