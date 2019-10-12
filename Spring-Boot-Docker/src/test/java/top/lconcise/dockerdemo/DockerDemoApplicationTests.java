68690b62355bc7696d01a4bd71157174a7cb2500
liusj <15114818657@163.com>
2019-10-12 09:35:30 +0800

    Spring-Boot-Docker

---
+++ Spring-Boot-Docker/src/test/java/top/lconcise/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package top.lconcise.dockerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
