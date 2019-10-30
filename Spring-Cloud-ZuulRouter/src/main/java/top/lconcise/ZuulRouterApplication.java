c8cf697a217939d62a2584a5a95fb3ecc59ce94d
liusj <15114818657@163.com>
2019-10-30 18:53:03 +0800

    Spring-Cloud-ZuulRouter

--- Spring-Cloud-ZuulRouter/src/main/java/top/lconcise/ZuulRouterApplication.java
+++ Spring-Cloud-ZuulRouter/src/main/java/top/lconcise/ZuulRouterApplication.java
@@ -2,14 +2,23 @@ package top.lconcise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class ZuulRouterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulRouterApplication.class, args);
     }
 
+    @GetMapping("/test/hello")
+    public String hello() {
+        return "hello zuul";
+    }
 }
