2f789be8f1ab1c23e7d6745b3e37e97fcd45772c
liusj <15114818657@163.com>
2020-01-15 11:16:23 +0800

    Spring-Boot-Admin

---
+++ Spring-Boot-Admin-Demo/spring-boot-admin-client/src/main/java/top/lconcise/admin/client/AdminClientApplication.java
@@ -0,0 +1,15 @@
+package top.lconcise.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liusj on 2020/1/15
+ */
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(AdminClientApplication.class,args);
+    }
+}
---
+++ Spring-Boot-Admin-Demo/spring-boot-admin-server/src/main/java/top/lconcise/admin/server/AdminServerApplication.java
@@ -0,0 +1,18 @@
+package top.lconcise.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liusj on 2020/1/15
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
