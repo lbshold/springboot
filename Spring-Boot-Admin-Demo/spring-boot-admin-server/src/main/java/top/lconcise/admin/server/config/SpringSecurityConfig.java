02c7c16e5511b98801f8fd202eb6e153e5934b87
liusj <15114818657@163.com>
2020-01-15 16:29:35 +0800

    Spring-Boot-Admin-Eureka

---
+++ Spring-Boot-Admin-Demo/spring-boot-admin-server/src/main/java/top/lconcise/admin/server/config/SpringSecurityConfig.java
@@ -0,0 +1,41 @@
+package top.lconcise.admin.server.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * Created by liusj on 2020/1/15
+ */
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+    public SpringSecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .antMatchers("/systemConfig/**").permitAll()
+                .antMatchers("/serveConfig/**").permitAll()
+//                .antMatchers("/admin/instances/*/actuator/metrics/process.uptime").permitAll()
+                .antMatchers("/admin/instances/*/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
+
+}
--- Spring-Boot-Admin-Demo/spring-cloud-eureka-server/src/main/java/top/lconcise/eureka/server/EurekaServerApplication.java
+++ Spring-Boot-Admin-Demo/spring-cloud-eureka-server/src/main/java/top/lconcise/eureka/server/EurekaServerApplication.java
@@ -1,17 +1,17 @@
-package top.lconcise.admin.client;
+package top.lconcise.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by liusj on 2020/1/15
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-public class AdminClientApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args){
-        SpringApplication.run(AdminClientApplication.class,args);
+        SpringApplication.run(EurekaServerApplication.class,args);
     }
 }
