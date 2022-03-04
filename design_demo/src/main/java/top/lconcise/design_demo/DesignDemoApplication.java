ec8a0fa7b8f46f66517abc4c8cc69bfb8f78288a
15114818657@163.com <605715694>
2022-03-04 15:50:15 +0800

    init

---
+++ design_demo/src/main/java/top/lconcise/design_demo/DesignDemoApplication.java
@@ -0,0 +1,13 @@
+package top.lconcise.design_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignDemoApplication.class, args);
+	}
+
+}
