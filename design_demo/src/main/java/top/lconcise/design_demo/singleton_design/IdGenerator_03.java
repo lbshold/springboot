81755570d9ff81f44af0c57e3d9021bef28f5428
15114818657@163.com <605715694>
2022-03-07 14:59:21 +0800

    单例设计模式-5种实现方法

---
+++ design_demo/src/main/java/top/lconcise/design_demo/singleton_design/IdGenerator_03.java
@@ -0,0 +1,33 @@
+package top.lconcise.design_demo.singleton_design;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 双重检测.
+ *
+ * @author: liusj
+ * @date: 2022/3/4
+ */
+public class IdGenerator_03 {
+
+    private AtomicLong id = new AtomicLong(0);
+    private static IdGenerator_03 instance;
+
+    private IdGenerator_03() {
+    }
+
+    public static IdGenerator_03 getInstance() {
+        if (instance == null) {
+            synchronized (IdGenerator_03.class) {
+                if (instance == null) {
+                    instance = new IdGenerator_03();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Long getId() {
+        return id.getAndIncrement();
+    }
+}
---
+++ design_demo/src/main/java/top/lconcise/design_demo/singleton_design/IdGenerator_04.java
@@ -0,0 +1,27 @@
+package top.lconcise.design_demo.singleton_design;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author: liusj
+ * @date: 2022/3/4
+ */
+public class IdGenerator_04 {
+
+    private AtomicLong id = new AtomicLong(0);
+
+    private IdGenerator_04() {
+    }
+
+    private static class IdGenerator_04_inner {
+        private static final IdGenerator_04 instance = new IdGenerator_04();
+    }
+
+    public static IdGenerator_04 getInstance() {
+        return IdGenerator_04_inner.instance;
+    }
+
+    public Long getId() {
+        return id.getAndIncrement();
+    }
+}
