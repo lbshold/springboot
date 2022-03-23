13b63c3912611b4e93111d46d5a6ebffafe9d9d8
15114818657@163.com <605715694>
2022-03-23 18:16:05 +0800

    建造者模式

---
+++ design_demo/src/main/java/top/lconcise/design_demo/design_mode/creaction/factory_design/builder/Demo.java
@@ -0,0 +1,17 @@
+package top.lconcise.design_demo.design_mode.creaction.factory_design.builder;
+
+/**
+ * @author: liusj
+ * @date: 2022/3/23
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        ResourcePoolConfig config = new ResourcePoolConfig.Builder()
+                .setName("dbConnectionPool")
+                .setMaxTotal(16)
+                .setMaxIdle(10)
+                .setMinIdle(12)
+                .build();
+    }
+}
---
+++ design_demo/src/main/java/top/lconcise/design_demo/design_mode/creaction/factory_design/builder/ResourcePoolConfig.java
@@ -0,0 +1,79 @@
+package top.lconcise.design_demo.design_mode.creaction.factory_design.builder;
+
+import lombok.Data;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author: liusj
+ * @date: 2022/3/23
+ */
+@Data
+public class ResourcePoolConfig {
+    private String name;
+    private int maxTotal;
+    private int maxIdle;
+    private int minIdle;
+
+    private ResourcePoolConfig(Builder builder) {
+        this.name = builder.name;
+        this.minIdle = builder.minIdle;
+        this.maxIdle = builder.maxIdle;
+        this.minIdle = builder.minIdle;
+    }
+
+    public static class Builder {
+        private static final int DEFAULT_MAX_TOTAL = 8;
+        private static final int DEFAULT_MAX_IDLE = 8;
+        private static final int DEFAULT_MIN_IDLE = 0;
+
+        private String name;
+        private int maxTotal = DEFAULT_MAX_TOTAL;
+        private int maxIdle = DEFAULT_MAX_IDLE;
+        private int minIdle = DEFAULT_MIN_IDLE;
+
+        public ResourcePoolConfig build() {
+            if (StringUtils.isEmpty(name)) {
+                throw new IllegalArgumentException("...");
+            }
+            if (maxIdle > maxTotal) {
+                throw new IllegalArgumentException("...");
+            }
+            if (minIdle > maxTotal || minIdle > maxIdle) {
+                throw new IllegalArgumentException("...");
+            }
+            return new ResourcePoolConfig(this);
+        }
+
+        public Builder setName(String name) {
+            if (StringUtils.isEmpty(name)) {
+                throw new IllegalArgumentException("...");
+            }
+            this.name = name;
+            return this;
+        }
+
+        public Builder setMaxTotal(int maxTotal) {
+            if (maxTotal < 0) {
+                throw new IllegalArgumentException("...");
+            }
+            this.maxTotal = maxTotal;
+            return this;
+        }
+
+        public Builder setMaxIdle(int maxIdle) {
+            if (maxIdle < 0) {
+                throw new IllegalArgumentException("...");
+            }
+            this.maxIdle = maxIdle;
+            return this;
+        }
+
+        public Builder setMinIdle(int minIdle) {
+            if (minIdle < 0) {
+                throw new IllegalArgumentException("...");
+            }
+            this.minIdle = minIdle;
+            return this;
+        }
+    }
+}
